18b72abb2027f12f1a455e630e49dfda10d8eb85
Mauro Alemán <mauro.aleman@numrot.com>
2023-02-17 13:37:31 -0500

    Se coloca el titulo de la pagina en minuscula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-// import { AuthComponent } from './components/shared/auth/auth.component';
 import { AngularPageComponent } from './components/angular-page/angular-page.component';
 import { SharedModule } from '../app/components/shared/shared.module';
 
