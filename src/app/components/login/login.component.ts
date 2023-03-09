94c13423510ad6b7d00b9a26efebc0db57f7ae37
Mauro Alemán <mauro.aleman@numrot.com>
2023-03-09 13:17:07 -0500

    se agrega la función de conectar con metamask al boton Start

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from 'src/app/services/loader.service';
+import { AuthWeb3Service } from 'src/app/services/auth/auth-web3.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./style.css']
 })
 export class LoginComponent implements OnInit {
 
   loading:boolean = false;
 
-  constructor(private loadingService:LoadingService){
+  constructor(private loadingService:LoadingService, private authWeb3Service: AuthWeb3Service){
   }
+
 
   ngOnInit(): void {
 
@@ -20,7 +23,8 @@ export class LoginComponent implements OnInit {
     
   }
 
-  copy(){
-    
-  }
+  connect(){
+    this.authWeb3Service.connect();
+  };
+  
 }
